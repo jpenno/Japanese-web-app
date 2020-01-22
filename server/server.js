cdd8fa776266c3e666ae191fad1bb83c1fc80138
jpenno <jpenno123@hotmail.com>
2020-01-22 15:02:05 +1100

    add get Kanji get rout

--- server/server.js
+++ server/server.js
@@ -3,11 +3,26 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const db = require("./db");
-
+const collection = require("../config/keys").Collection;
 const app = express();
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "../", "public")));
+
+app.get("/Kanji", (req, res) => {
+  db.getDB()
+    .collection(collection)
+    .find({})
+    .toArray((err, documents) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(documents);
+        res.json(documents);
+      }
+    });
+});
+
 
 db.connect(err => {
   if (err) {
